62188ea0b6aa04c0fa5df9371e2e548d4ee27f44
Diwank Singh <diwank.singh@gmail.com>
2013-10-23 22:53:56 -0700

    Added pictures and made supersized work
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,54 +1,25 @@
-$(document).ready(function() {
-			var controller = $.superscrollorama();
-			// individual element tween examples
-			
-			// header
-			controller.addTween('#header',
-	            TweenMax.fromTo( $('#header'), .5,
-	                                {css:{fontSize:'120px'},
-	                                immediateRender:true,
-	                                ease:Quad.easeInOut},
+jQuery(function($) {
+  $.supersized({
+    // Options
+    keyboard_nav: 0,
+    random: 1,
+    transition_speed: 1000,
 
-	                                {css:{fontSize:'90px'},
-	                                ease:Quad.easeInOut}
-	                            )
-                        );
-            
-            // social
-            controller.addTween('#social',
-	            TweenMax.to( $('#social'), .25,
-	                                {css:{fontSize:'25px'},
-	                                immediateRender:true,
-	                                ease:Quad.easeInOut}
-	                            )
-                        );
-            
-            // label
-			controller.addTween('#label',
-		        TweenMax.to( $('#label'), .75, {css:{opacity: 0}})
-        		    );
-		    
-		    // story elements
-		    var story = $('#story h2');
-		    for(var i = 1; i <= story.length; i++) {
-			if(i%2)
-			controller.addTween('#line-'+i,
-		        TweenMax.from( $('#line-'+i), .25, 
-		                        {css:{right:'25%', opacity: 0, fontSize:'40px'},
-		                        ease:Quad.easeInOut}
-	                          )
-	    	        );
-	        else
-	        controller.addTween('#line-'+i,
-		        TweenMax.from( $('#line-'+i), .25, 
-		                        {css:{left:'25%', opacity: 0, fontSize:'40px'},
-		                        ease:Quad.easeInOut}
-	                          )
-	    	        );
-	        }
-	        
-	        // label2
-			controller.addTween('#label2',
-		        TweenMax.from( $('#label2'), .75, {css:{opacity: 0}})
-        		    );
-		});
+    // Defaults
+    slide_interval: 5000,
+    autoplay: true,
+    transition: 1, // fade
+
+    slides: (function(max) {
+      var i, _slides = [];
+
+      for(i=1; i <= max; i = ++i) {
+        _slides.push({
+          image: 'img/pics/' + i + '.jpg'
+        });
+      }
+
+      return _slides;
+    })(19) // Number of images
+  });
+});

ce03a53cb21515b026ff2d1d4f778fd29b98a583
Diwank Singh <diwank.singh@gmail.com>
2013-10-24 00:10:26 -0700

    Added sepia filter to portrait and text.
    Signed-off-by: Diwank Singh <diwank.singh@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,25 +1,9 @@
 jQuery(function($) {
-  $.supersized({
-    // Options
-    keyboard_nav: 0,
-    random: 1,
-    transition_speed: 1000,
+  var selector = '.headshot img',
+      image = Filtrr2(selector);
 
-    // Defaults
-    slide_interval: 5000,
-    autoplay: true,
-    transition: 1, // fade
-
-    slides: (function(max) {
-      var i, _slides = [];
-
-      for(i=1; i <= max; i = ++i) {
-        _slides.push({
-          image: 'img/pics/' + i + '.jpg'
-        });
-      }
-
-      return _slides;
-    })(19) // Number of images
+  image.ready(function() {
+    this.sepia().expose(-20).render();
+    $(image.canvas).addClass('circular');
   });
 });
